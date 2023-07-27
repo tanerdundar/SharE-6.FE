14fd7136a6dc1a291621672bdb51f1aacffcac1b
tanerdundar <t.dundar145@gmail.com>
2023-07-27 12:42:34 +0300

    try netlify

--- SharE-6/src/components/SignIn.jsx
+++ SharE-6/src/components/SignIn.jsx
@@ -114,7 +114,7 @@ export default function SignIn(props) {
                 <LockOutlinedIcon />
               </Avatar>
               <Typography component="h1" variant="h5">
-                Sign in
+                Sign in deneme
               </Typography>
               <Box
                 component="form"
